a235253c151a4f663d3721e3f5ca9a8f5df074a0
ilir lika <49962713+1ilir0lika@users.noreply.github.com>
2024-02-25 13:50:36 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -11,3 +11,30 @@ def move_player(player):
         player.position -= len(set_board.board_positions)
         player.cash += 200
     print("la posizione é "+str(player.position))
+    #match the type of the position and do the corresponding action
+    match type(set_board.board_positions[player.position]):
+        case classi.Property:
+            if set_board.board_positions[player.position].owner == None:
+                print("Vuoi comprare "+set_board.board_positions[player.position].name+" per "+str(set_board.board_positions[player.position].price)+"?")
+                answer = input("si/no")
+                if answer == "si":
+                    set_board.board_positions[player.position].buy(player)
+            else:
+                print("Devi pagare "+str(set_board.board_positions[player.position].rent)+" a "+set_board.board_positions[player.position].owner)
+                for i in range(len(set_board.players)):
+                    if set_board.players[i].name == set_board.board_positions[player.position].owner:
+                        set_board.players[i].cash += set_board.board_positions[player.position].rent
+                        player.cash -= set_board.board_positions[player.position].rent
+        case classi.Imprevisto:
+            print("Hai pescato una carta imprevisto")
+        case classi.Probabilita:
+            print("Hai pescato una carta probabilità")
+        case classi.Tax:
+            print("Devi pagare "+str(set_board.board_positions[player.position].amount)+" di tasse")
+            player.cash -= set_board.board_positions[player.position].amount
+        case classi.Jail:
+            print("Sei in prigione")
+            player.jail = True
+            player.position = 10
+        case _:
+            print("Non é possibile fare nulla")
