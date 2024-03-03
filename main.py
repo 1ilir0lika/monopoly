28083dfb5496e1dd04df35db6e9ecedf4b6599ee
ilir lika <49962713+1ilir0lika@users.noreply.github.com>
2024-03-03 12:26:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,28 +5,30 @@ import functions
 import set_board
 # Initialize Pygame
 pygame.init()
-# Set background color
-set_board.screen.fill((0, 128, 0), (0, 0, set_board.width_window, set_board.height_window))
-# Display the game board
-set_board.screen.blit(set_board.board, (25, 25))
-# Display the title of the game
-font = pygame.font.SysFont('Times New Roman', 150)
-text = font.render('MONOPOLY', True, (255, 255, 255))
-text_rect = text.get_rect()
-text_rect.center = (set_board.width_game + set_board.width_window / 5, 150)
-set_board.screen.blit(text, text_rect)
-functions.display_players()
-# Update the display
-functions.display_stats()
-pygame.display.update()
-# Clear the screen for the next iteration
-set_board.screen.fill((0, 128, 0), (0, 0, set_board.width_window, set_board.height_window))
-# Main game loop
+
 while set_board.game_running:
-    # Handle events
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-    # Move the player
-    functions.move_player(set_board.players[0])
+        # Set background color
+        set_board.screen.fill((0, 128, 0), (0, 0, set_board.width_window, set_board.height_window))
+        # Display the game board
+        set_board.screen.blit(set_board.board, (25, 25))
+        #add image it.png inside the game window
+        it = pygame.image.load('it.png')
+        it = pygame.transform.scale(it, (100, 70))
+        set_board.screen.blit(it, (100,1300))
+        # Display the title of the game
+        font = pygame.font.SysFont('Times New Roman', 150)
+        text = font.render('MONOPOLY', True, (255, 255, 255))
+        text_rect = text.get_rect()
+        text_rect.center = (set_board.width_game + set_board.width_window / 5, 150)
+        set_board.screen.blit(text, text_rect)
+        functions.display_players()
+        functions.display_stats()
+        functions.move_player(set_board.players[0])
+        pygame.display.update()
+pygame.display.update()
+pygame.time.delay(1000)
+pygame.quit()
